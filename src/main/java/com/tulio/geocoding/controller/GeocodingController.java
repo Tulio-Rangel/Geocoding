dab313ce0ef5629324c332e816872e5985c65281
Tulio-Rangel <tjrn20@hotmail.com>
2023-05-06 18:48:44 -0500

    second point done, but neighborhood is null

--- src/main/java/com/tulio/geocoding/controller/GeocodingController.java
+++ src/main/java/com/tulio/geocoding/controller/GeocodingController.java
@@ -2,10 +2,12 @@ package com.tulio.geocoding.controller;
 
 import com.google.maps.model.LatLng;
 import com.tulio.geocoding.service.GeocodingService;
+import com.tulio.geocoding.service.NeighborhoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GeocodingController {
@@ -13,9 +15,31 @@ public class GeocodingController {
     @Autowired
     private GeocodingService geocodingService;
 
-    @GetMapping("/geocode")
+    @Autowired
+    private NeighborhoodService neighborhoodService;
+
+    /*@GetMapping("/geocode")
     public LatLng geocode(@RequestParam String address) throws Exception {
         return geocodingService.geocodeAddress(address);
+    }*/
+
+    @GetMapping("/geocode")
+    public GeocodeResult geocode(@RequestParam String address) throws Exception {
+        LatLng latLng = geocodingService.geocodeAddress(address);
+        String neighborhood = neighborhoodService.getNeighborhoodName(latLng.lat, latLng.lng);
+        return new GeocodeResult(latLng.lat, latLng.lng, neighborhood);
+    }
+
+    private static class GeocodeResult {
+        public double lat;
+        public double lng;
+        public String neighborhood;
+
+        public GeocodeResult(double lat, double lng, String neighborhood) {
+            this.lat = lat;
+            this.lng = lng;
+            this.neighborhood = neighborhood;
+        }
     }
 }
 
--- src/main/java/com/tulio/geocoding/service/NeighborhoodService.java
+++ src/main/java/com/tulio/geocoding/service/NeighborhoodService.java
@@ -1,16 +1,20 @@
 package com.tulio.geocoding.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import com.tulio.geocoding.config.AppConfig;
 
 @Service
+@Import(AppConfig.class)
 public class NeighborhoodService {
 
     private static final String ENDPOINT_URL = "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/USA_Neighborhoods/FeatureServer/0/query";
 
     @Autowired
     private RestTemplate restTemplate;
+
 
     public String getNeighborhoodName(double latitude, double longitude) {
         String where = String.format("contains(geom, POINT(%f %f))", longitude, latitude);
